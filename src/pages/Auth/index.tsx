373b00711fe28ed765f87aadb38468ef06dad797
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-01-17 06:25:24 -0600

    Add converted TypeScript files from 'site'

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,43 +1,41 @@
-// temporary
-/* eslint-disable */
-// @ts-nocheck
-
 import React from 'react';
 import queryString from 'query-string';
 import { useLocation } from 'react-router-dom';
 
-import { getToken } from 'api';
+import { getToken } from 'util/api';
 // import Loading from 'components/Loading';
 
-function mobileRedirect(os, code) {
+function mobileRedirect(os: 'android' | 'ios', code: string) {
   const to = `hackillinois://org.hackillinois.${os}/auth?code=${code}`;
   window.location.replace(to);
 }
 
-const Auth = () => {
+const Auth: React.FC = () => {
   const location = useLocation();
-  const {
-    code,
-    isAndroid,
-    isiOS,
-    to,
-  } = queryString.parse(location.search);
 
-  if (!code) {
+  type QueryTypes = {
+    code?: string;
+    isAndroid?: string;
+    isiOS?: string;
+    to?: string;
+  };
+  const { code, isAndroid, isiOS, to }: QueryTypes = queryString.parse(location.search);
+
+  if (code) {
+    if (isAndroid || isiOS) {
+      const os = isAndroid ? 'android' : 'ios';
+      mobileRedirect(os, code);
+    } else {
+      getToken(code).then((token) => {
+        sessionStorage.setItem('token', token);
+        window.location.replace(to as string);
+      });
+    }
+  } else {
     window.location.replace('/');
   }
 
-  if (isAndroid || isiOS) {
-    const os = isAndroid ? 'android' : 'ios';
-    mobileRedirect(os, code);
-  } else {
-    getToken(code).then((token) => {
-      sessionStorage.setItem('token', token);
-      window.location.replace(to);
-    });
-  }
-
   return <div>Loading...</div>; // <Loading />;
-}
+};
 
 export default Auth;

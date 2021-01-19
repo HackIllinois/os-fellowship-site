83bf17b0407b88f69a21582383345965b280d3c8
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-01-19 15:23:30 -0600

    Update google analytics settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactGA.initialize('G-2D2S4GLZPR', {
+ReactGA.initialize('G-TY8QF5NK77', {
   testMode: process.env.NODE_ENV !== 'production',
 });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );

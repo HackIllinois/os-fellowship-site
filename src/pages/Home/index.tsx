2aaaf086e000db526e8a1a7b20fba919bd9e1551
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-01-21 06:17:57 -0600

    Replace gif animation with Lottie

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,6 +9,7 @@ import styles from './styles.module.scss';
 const Home: React.FC = () => (
   <div className={styles.home}>
     <Hero />
+    <div className={styles.landscapeExtension} />
     <EventInfo />
     <FAQ />
   </div>
